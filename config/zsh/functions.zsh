934ec133615f1ee5742e53ea6fbf782f8965396d
Asen Lekov <asenlekoff@gmail.com>
2017-01-11 01:57:32 +0200

    Added key hash function for android projects

--- config/zsh/functions.zsh
+++ config/zsh/functions.zsh
@@ -3,6 +3,10 @@ function zssh() {
   ssh "$@" -t zsh
 }
 
-function hgrep () { 
+function hgrep () {
     history | egrep --color=auto --recursive "$@" | egrep --color=auto --recursive -v "hgrep $@"
+}
+
+function keyhash() {
+  keytool -exportcert -alias $2 -keystore $1 | openssl sha1 -binary | openssl base64
 }
