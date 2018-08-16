a6e94edd7eb3edd058268bf038380083c40bd955
Asen Lekov <asenlekoff@gmail.com>
2018-08-16 18:32:01 +0300

    Update brews
    Add cask cleanup

--- scripts/brews.sh
+++ scripts/brews.sh
@@ -97,6 +97,7 @@ brew cask install alfred
 
 # Remove outdated versions from the Cellar
 brew cleanup
+brew cask cleanup
 
 echo "Go to https://www.bresink.com/osx/0TinkerTool/download.php to download TinkerTool"
 echo "Setup GPG siging using keybase. Folow https://github.com/pstadler/keybase-gpg-github"
